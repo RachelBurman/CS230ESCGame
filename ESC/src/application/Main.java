e1fcc58c2ed3680c8c8dca9a7d4b201024eae443
MattEl123 <57944700+MattEl123@users.noreply.github.com>
2019-11-22 14:00:28 +0000

    Update Main.java

--- ESC/src/application/Main.java
+++ ESC/src/application/Main.java
@@ -3,15 +3,28 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			Pane pane = new HBox(15);
+		
+			Image cellImage = new Image("https://images.unsplash.com/photo-1533035353720-f1c6a75cd8ab?ixlib=rb-1.2.1&w=1000&q=80");
+			ImageView cellImage2 = new ImageView(cellImage);
+			cellImage2.setFitHeight(32);
+			cellImage2.setFitWidth(32);
+			pane.getChildren().add(cellImage2);
+            
+			
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(pane,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
