a68de0c00377e8f8407665c0d41b0e0e6133ccb4
jkwok678 <jkwok678@gmail.com>
2019-11-26 18:03:39 +0000

    Update NonTargetingEnemy.java

--- ESC/src/application/NonTargetingEnemy.java
+++ ESC/src/application/NonTargetingEnemy.java
@@ -1,5 +1,10 @@
 package application;
 
-public class NonTargetingEnemy {
+public class NonTargetingEnemy extends Enemy {
+
+	public NonTargetingEnemy(int currentXLocation, int currentYLocation, String enemyName) {
+		super(currentXLocation, currentYLocation, enemyName);
+		// TODO Auto-generated constructor stub
+	}
 
 }
