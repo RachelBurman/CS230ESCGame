ad87424c31cfee8b7d9b8c0728b6133972eea5da
MattEl123 <57944700+MattEl123@users.noreply.github.com>
2019-11-26 02:41:31 +0000

    Create Water.java

---
+++ ESC/src/application/Water.java
@@ -0,0 +1,17 @@
+package application;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Water extends CellType{
+
+	public Water() {
+            super.setImage("https://www.bhp.com/-/media/images/2018-water-report/180828_wr2018cover.jpg?mh=844&h=844&w=1500&la=en&hash=16406F779CD04F31FE68780D8A645746DB1A5A0C");
+	}
+	
+	public ImageView getWaterImage() {
+		
+		return super.getCellImage();
+		
+	}
+}
