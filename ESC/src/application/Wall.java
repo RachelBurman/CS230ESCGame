77df680bf9219353b0166a6aef347da865082412
MattEl123 <57944700+MattEl123@users.noreply.github.com>
2019-11-26 02:41:54 +0000

    Create Wall.java

---
+++ ESC/src/application/Wall.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.scene.image.ImageView;
+
+public class Wall extends CellType {
+	
+	public Wall() {
+            
+            super.setImage("https://images.unsplash.com/photo-1520758594221-872948699332?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80");
+            
+	}
+	
+	public ImageView getWallImage() {
+		
+		return super.getCellImage();
+		
+	}
+}
