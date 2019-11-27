67bfa52f93909963f7b0cc6c63992949e193f909
AnnriyaR <57924490+AnnriyaR@users.noreply.github.com>
2019-11-27 18:33:54 +0000

    Add files via upload

---
+++ ESC/src/application/Door.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Door extends CellType {
+	
+	Image DoorColour;
+	ImageView DoorColour2;
+	
+	
+	public Door() { 
+		
+		super.setImage("red door.jpg, green door.jpg, yellow door.jpg, blue door.jpg");
+		
+	}
+	
+	public ImageView getGoalImage() {
+		
+		return super.getCellImage();
+	}
+
+}
+
