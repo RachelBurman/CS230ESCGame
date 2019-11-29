62af98904661bd3278d2afbe9ba00062043ed6d1
macie <macie@137.44.126.40>
2019-11-29 15:34:50 +0000

    Load level first changes, and user profile

--- ESC/src/application/LoadLevel.java
+++ ESC/src/application/LoadLevel.java
@@ -5,44 +5,45 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class LoadLevel {
-	public static char[][] loadLevel(String file) {
-	/// public static Cell[][] loadLevel(String file) {
+	//public static char[][] loadLevel(String file) {
+	public static CellType[][] loadLevel(String file) {
 		try
 		{
 			File f = new File(file);
 			Scanner in = new Scanner(f);
 			int x = in.nextInt();
 			int y = in.nextInt();
-			//Cell[][] level = new Cell[][];
-			char[][] level = new char[x][y];
+			CellType[][] level = new CellType[x][y];
+			//char[][] level = new char[x][y];
 			in.nextLine();
 			for (int i = 0; i<x; i++) {
 				String s = in.nextLine();
 				for (int o = 0; o<y; o++) {
-					//if (s.charAt(o) == '#') {
-					//	level[i][o] = Wall;
-					//} else if (s.charAt(o) == ' ') {
-					//	level[i][o] = Ground;
-					//} else if (s.charAt(o) == 'G') {
-					//	level[i][o] = Goal;
-					//} else if (s.charAt(o) == 'W') {
-					//	level[i][o] = Water;
-					//} else if (s.charAt(o) == 'P') {
-					//	level[i][o] = Flippers;
-					//} else if (s.charAt(o) == 'F') {
-					//	level[i][o] = Fire;
-					//} else if (s.charAt(o) == 'B') {
-					//	level[i][o] = Boots;
-					//} else if (s.charAt(o) == 'S') {
-					//	level[i][o] = Start;
-					//} else if (s.charAt(o) == 'T') {
-					//	level[i][o] = Token;
-					level[i][o]=s.charAt(o);
+					if (s.charAt(o) == '#') {
+						level[i][o] = new Wall();
+					} else if (s.charAt(o) == ' ') {
+						level[i][o] = new Ground();
+					} else if (s.charAt(o) == 'G') {
+						level[i][o] = new Goal();
+					} else if (s.charAt(o) == 'W') {
+						level[i][o] = new Water();
+					} else if (s.charAt(o) == 'P') {
+						level[i][o] = new Flippers();
+					} else if (s.charAt(o) == 'F') {
+						level[i][o] = new Fire();
+					} else if (s.charAt(o) == 'B') {
+						level[i][o] = new Boots();
+					} else if (s.charAt(o) == 'S') {
+						level[i][o] = new Start();
+					} else if (s.charAt(o) == 'T') {
+						level[i][o] = new Token();
+					}
+					//level[i][o]=s.charAt(o);
 				}
 			}
-			//while (in.hasNextLine()) {
-			//This is where the more specified Cells and enemies will go	
-			//}
+			while (in.hasNextLine()) {
+				
+			}
 			in.close();
 			return level;
 		} 
