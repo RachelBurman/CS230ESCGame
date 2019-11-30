4f3f7770040e6a4cf94aca79069e168cb9405229
MattEl123 <57944700+MattEl123@users.noreply.github.com>
2019-11-30 16:11:42 +0000

    Update Main.java

--- ESC/src/application/Main.java
+++ ESC/src/application/Main.java
@@ -3,11 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
 
@@ -27,14 +23,65 @@ public class Main extends Application {
 			GridPane panes = new GridPane();
             panes.setVgap(1);
             panes.setHgap(1);
+            
+            int playerX = 0;
+            int playerY = 0;
 			
 			for (int i = 0; i<y; i++) {
 				
 				for (int o = 0; o<x; o++) {
-					panes.add(level[i][o].getCellImage(),o,i);
+					
+					if (level[i][o] instanceof Player) {
+						playerX = o;
+						playerY = i;
+						System.out.println(playerX);
+						System.out.println(playerY);
+					}
+					
+					//panes.add(level[i][o].getCellImage(),o,i);
 				}
 				
 			}
+			
+			int scopeXmin;
+			int scopeXmax;
+			int scopeYmin;
+			int scopeYmax;
+			
+			if (playerX - 3 < 0) {
+				scopeXmin = 0;
+			} else {
+				scopeXmin = playerX - 3;
+			}
+			if (playerX + 4 > x) {
+				scopeXmax = x;
+			} else {
+				scopeXmax = playerX + 4;
+			}
+			if (playerY - 3 < 0) {
+				scopeYmin = 0;
+			} else {
+				scopeYmin = playerY - 3;
+			}
+			if (playerY + 4 > y) {
+				//System.out.println(playerY);
+				//System.out.println(y);
+				scopeYmax = y;
+				//System.out.println(scopeYmax);
+			} else {
+				scopeYmax = playerY + 4;
+				System.out.println(scopeYmax);
+			}
+			
+			for (int i = scopeYmin; i<scopeYmax; i++) {
+				for (int o = scopeXmin; o<scopeXmax; o++) {
+					panes.add(level[i][o].getCellImage(),o,i);
+				}
+			}
+				
+				
+			
+			
 			
 			Pane rootPane = new Pane();
             rootPane.getChildren().addAll(panes);
