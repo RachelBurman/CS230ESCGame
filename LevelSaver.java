856a159e44406f4b28c57b478df2c2e36d20c56b
Rachel Burman <rachel.burman@gmail.com>
2019-12-06 16:12:49 +0000

    Add files via upload

---
+++ LevelSaver.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LevelSaver {
+	public static void player(String player){
+		
+		
+		
+//		BufferedReader br = new BufferedReader(new FileReader("Profiles.txt"));
+//		
+//		try{
+//			String player = br.readLine();
+//			
+//			while(player != null){
+//				player = br.readLine();
+//				String[] splitStr = player.split("  ");
+//				if(splitStr.length==4){
+//					String playerOne = splitStr[0];
+//					String playerTwo = splitStr[1];
+//					String playerThree = splitStr[2];
+//					String playerFour = splitStr[3];
+//					String playerFive = splitStr[4];
+//				}
+//			}
+//		}
+//		catch(Exception FileNotFound){
+//			System.exit(0);
+//		}
+	}
+	public static void FileMaker(String player, String playerOne) throws IOException{
+		if(player == playerOne){
+			
+		}else if(player == "playerTwo"){
+			
+		}else if(player == "playerThree"){
+			
+		}else if(player == "playerFour"){
+			
+		}else{
+			
+		}		
+	}
+		
+	public static String PlayerFile(int playerCoord, int enemyCoord){
+		
+		//create new file called whatever player number
+		//save player name
+		//save player time
+		//save player coordinates
+		//save map position
+		//save enemy position
+	}
+	
+	
+	
+}
