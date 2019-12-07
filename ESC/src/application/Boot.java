272d331566554059430cdbe151132285a04eb7d1
MattEl123 <57944700+MattEl123@users.noreply.github.com>
2019-12-07 21:40:27 +0000

    Update Boot.java

--- ESC/src/application/Boot.java
+++ ESC/src/application/Boot.java
@@ -7,6 +7,12 @@ import javafx.scene.image.ImageView;
 
 public class Boot extends Cell {
 
+	/*
+	* Creates a boot to be displayed onto the game map
+	* @param name name of the cell type
+	* @param xCoordinate the x coordinate of a boot cell
+	* @param yCoordinate the y coordinate of a boot cell
+	*/
 	public Boot(String name, int xCoordinate, int yCoordinate) {
 		super(name, xCoordinate, yCoordinate);
 		this.playerPass = true;
@@ -18,6 +24,11 @@ public class Boot extends Cell {
 		super.cellView.setFitWidth(CELL_SIZE);
 		
 	}
+	
+	/*
+	* Gets the image view class type of a boot cell to be displayed
+	* @return an image view with the given x and y coordinates
+	*/
 	public ImageView getBootView() {
 		
 		return super.getView();
