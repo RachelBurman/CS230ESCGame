148d48880f1b1fd24d86dcd448e6aadc692753d5
MattEl123 <57944700+MattEl123@users.noreply.github.com>
2019-12-07 22:14:23 +0000

    Update Teleporter.java

--- ESC/src/application/Teleporter.java
+++ ESC/src/application/Teleporter.java
@@ -5,9 +5,21 @@ import java.io.File;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/*
+* This class represents a teleporter at coordinates x and y
+* @author Group 31 
+* @version 5.0
+*/
 public class Teleporter extends Cell {
 	int linkX;
 	int linkY;
+	/*
+	* Creates a teleporter at the given x and y coordinates
+	* @param name the name of the teleporter
+	* @param xCoordinate the x coordinate of a teleporter
+	* @param yCoordinate the y coordinate of a teleporter
+	* @param file the file name of the level the teleporter is located in
+	*/
 	public Teleporter(String name,int xCoordinate, int yCoordinate, String file) {
 		
 		super(name, xCoordinate, yCoordinate);
