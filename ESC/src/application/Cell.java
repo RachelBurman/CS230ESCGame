a8012ca7fd769d1137d44171e52e471305764128
jkwok678 <jkwok678@gmail.com>
2019-12-07 13:43:52 +0000

    Size of tiles increased

--- ESC/src/application/Cell.java
+++ ESC/src/application/Cell.java
@@ -12,7 +12,7 @@ public class Cell {
 	String name;
 	Image cellImage;
 	ImageView cellView;
-	final int CELL_SIZE= 50;
+	final int CELL_SIZE= 75;
 	int xCoordinate;
 	int yCoordinate;
 	boolean playerPass;
