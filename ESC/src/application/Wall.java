77efafd88d6737a779dbf026112a724e3440eac3
MattEl123 <57944700+MattEl123@users.noreply.github.com>
2019-12-07 22:27:07 +0000

    Update Wall.java

--- ESC/src/application/Wall.java
+++ ESC/src/application/Wall.java
@@ -5,8 +5,19 @@ import java.io.File;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/*
+* This class represents a wall at coordinates x and y
+* @author Group 31 
+* @version 3.0
+*/
 public class Wall extends Cell{
 	
+	/*
+	* Creates a wall at the given x and y coordinate
+	* @param name the name of a wall cell
+	* @param xCoordinate the x coordinate of a wall cell
+	* @param yCoordinate the y coordinate of a wall cell
+	*/
 	public Wall (String name, int xCoordinate, int yCoordinate) {
 		super(name, xCoordinate, yCoordinate);
 		this.playerPass = false;
@@ -19,6 +30,11 @@ public class Wall extends Cell{
 		super.cellView.setFitWidth(CELL_SIZE);
 		
 	}
+	
+	/*
+	* Gets the image view of a wall cell
+	* @return an image viewer class of a wall cell
+	*/
 	public ImageView getWallView() {
 		
 		return super.getView();
