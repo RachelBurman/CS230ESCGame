df7a06ac3160b3e8d09e41c409e44fc85bfb260a
MattEl123 <57944700+MattEl123@users.noreply.github.com>
2019-12-07 22:19:57 +0000

    Update Token.java

--- ESC/src/application/Token.java
+++ ESC/src/application/Token.java
@@ -5,8 +5,19 @@ import java.io.File;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/*
+* This class represents a token at coordinates x and y
+* @author Group 31 
+* @version 2.0
+*/
 public class Token extends Cell {
 
+	/*
+	* Creates a token at the given x and y coordinates
+	* @param name the name of a token
+	* @param xCoordinate the x coordinate of a token cell
+	* @param yCoordinate the y coordinate of a token cell
+	*/
 	public Token(String name, int xCoordinate, int yCoordinate) {
 		super(name, xCoordinate, yCoordinate);
 		this.playerPass = true;
@@ -18,6 +29,11 @@ public class Token extends Cell {
 		super.cellView.setFitWidth(CELL_SIZE);
 		
 	}
+	
+	/*
+	* Gets the image view of a token
+	* @return the image viewer cell type of a token
+	*/
 	public ImageView getTokenView() {
 		
 		return super.getView();
