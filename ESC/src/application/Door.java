4a810842d3b414198aa210167e78f0c0ce3501f3
MattEl123 <57944700+MattEl123@users.noreply.github.com>
2019-12-07 21:44:13 +0000

    Update Door.java

--- ESC/src/application/Door.java
+++ ESC/src/application/Door.java
@@ -5,8 +5,19 @@ import java.io.File;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/*
+* This class represents a door at coordinates x and y
+* @author Group 31 
+* @version 2.0
+*/
 public class Door extends Cell {
 
+	/*
+	* Creates a door at the given x and y coordinates
+	* @param name the colour code of a door
+	* @param xCoordinate the x coordinate of a door cell
+	* @param yCoordinate the y coordinate of a door cell
+	*/
 	public Door(String name, int xCoordinate, int yCoordinate) {
 		super(name, xCoordinate, yCoordinate);
 		this.name = name;
