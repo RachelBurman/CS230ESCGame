7537725017c8acdf23215f35ed0ca05aa5bcc7a1
MattEl123 <57944700+MattEl123@users.noreply.github.com>
2019-12-07 22:30:45 +0000

    Update Water.java

--- ESC/src/application/Water.java
+++ ESC/src/application/Water.java
@@ -6,8 +6,19 @@ import java.io.File;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/*
+* This class represents a water cell at coordinates x and y
+* @author Group 31 
+* @version 2.0
+*/
 public class Water extends Cell {
 
+	/*
+	* Creates a water cell at the given x and y coordinates
+	* @param name the name of a water cell
+	* @param xCoordinate the x coordinate of a water cell
+	* @param yCoordinate the y coordinate of a water cell
+	*/
 	public Water(String name,int xCoordinate, int yCoordinate) {
 		super(name,xCoordinate, yCoordinate);
 		// TODO Auto-generated constructor stub
@@ -20,6 +31,11 @@ public class Water extends Cell {
 		super.cellView.setFitWidth(CELL_SIZE);
 		
 	}
+	
+	/*
+	* Gets the image view of a water cell
+	* @return an image viewer class of a water cell
+	*/
 	public ImageView getWaterView() {
 		
 		return super.getView();
