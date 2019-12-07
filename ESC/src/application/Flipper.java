bf2f1bfff844d36793bbc8e061293e8c5b535591
MattEl123 <57944700+MattEl123@users.noreply.github.com>
2019-12-07 21:51:22 +0000

    Update Flipper.java

--- ESC/src/application/Flipper.java
+++ ESC/src/application/Flipper.java
@@ -5,8 +5,19 @@ import java.io.File;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/*
+* This class represents a flipper at coordinates x and y
+* @author Group 31 
+* @version 2.0
+*/
 public class Flipper extends Cell {
 
+	/*
+	* Creates a flipper at the given x and y coordinates
+	* @param name the name of a flipper
+	* @param xCoordinate the x coordinate of a flipper cell
+	* @param yCoordinate the y coordinate of a flipper cell
+	*/
 	public Flipper(String name, int xCoordinate, int yCoordinate) {
 		super(name, xCoordinate, yCoordinate);
 		this.playerPass = true;
@@ -18,6 +29,11 @@ public class Flipper extends Cell {
 		super.cellView.setFitWidth(CELL_SIZE);
 		
 	}
+	
+	/*
+	* Gets the image view of a flipper
+	* @return a image viewer class type of a flipper
+	*/
 	public ImageView getFlipperView() {
 		
 		return super.getView();
