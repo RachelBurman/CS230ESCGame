ab18f245150820fe3687bf009597f34c5dda95f6
MattEl123 <57944700+MattEl123@users.noreply.github.com>
2019-12-08 16:21:30 +0000

    javadoc fixed

--- ESC/src/application/Fire.java
+++ ESC/src/application/Fire.java
@@ -4,14 +4,14 @@ import java.io.File;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-/*
+/**
 * This class represents a fire cell at coordinates x and y
 * @author Group 31 
 * @version 2.0
 */
 public class Fire extends Cell {
 
-	/*
+	/**
 	* Creates a fire cell at the given x and y coordinates
 	* @param name the name of the cell
 	* @param xCoordinate the x coordinate of a fire cell
@@ -32,7 +32,7 @@ public class Fire extends Cell {
 		
 	}
 	
-	/*
+	/**
 	* Gets the image view of a fire cell
 	* @return the image viewer class type of a fire cell
 	*/
