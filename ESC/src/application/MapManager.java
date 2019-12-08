923d53e52510a50310a63bb5b723ad7584a36580
jkwok678 <jkwok678@gmail.com>
2019-12-08 20:48:44 +0000

    Update MapManager.java

--- ESC/src/application/MapManager.java
+++ ESC/src/application/MapManager.java
@@ -12,7 +12,10 @@ public class MapManager {
 	private MapManager() {
 		
 	}
-	
+	/**
+	 * Make a map manager.
+	 * @return a mapManager object
+	 */
 	public static MapManager sharedMapManager() {
 		
 		if (onlyMapManager == null) {
@@ -20,19 +23,31 @@ public class MapManager {
 		}
 		return onlyMapManager;
 	}
-	
+	/**
+	 * Return the map.
+	 * @return map of game
+	 */
 	public Map getMap() {
 		return this.map;
 	}
-	
+	/**
+	 * Set the map.
+	 * @param map of game
+	 */
 	public void setMap(Map map) {
 		this.map = map;
 	}
-
+	/**
+	 * Returns GridPane of the game
+	 * @return grid
+	 */
 	public GridPane getGrid() {
 		return grid;
 	}
-
+	/***
+	 * Set the GridPane of the game
+	 * @param gridpane
+	 */
 	public void setGrid(GridPane grid) {
 		this.grid = grid;
 	}
